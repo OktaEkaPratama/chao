099594ebef242f601309ac18776d7f35da9872c4
OktaEkaPratama <oktapratama@students.amikom.ac.id>
2021-07-12 22:08:40 +0700

    project i

---
+++ matrix/perkalian.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    int x, y, a[4][5], b[4][5];
+
+    cout << "Perkalian Matrix 2 Dimensi" << endl;
+
+    cout << "Matrix 1" << endl;
+    for (x = 0; x <= 3; x++) {
+        for (y = 0; y <= 4; y++) {
+            cout << "Masukkan Index 1 (" << x << "," << y << ") = ";
+            cin >> a[x][y];
+        }
+    }
+    cout << endl << "Matrix 2" << endl;
+    for (x = 0; x <= 3; x++) {
+        for (y = 0; y <= 4; y++) {
+            cout << "Masukkan Index 2 (" << x << "," << y << ") = ";
+            cin >> b[x][y];
+        }
+    }
+    cout << endl << "Matrix 1 + Matrix 2" << endl;
+    for (x = 0; x <= 3; x++) {
+        cout << endl;
+        for (y = 0; y <= 4; y++) {
+            cout << a[x][y] * b[x][y] << " ";
+        }
+    }
+    return 0;
+}
